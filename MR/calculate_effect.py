f4ec5697a950023bf4f57accef55af761540fb75
Adam Chen <adamchen2301@gmail.com>
2024-01-15 15:35:05 +1100

    Added maximum likelihood method of effect estimation

--- MR/calculate_effect.py
+++ MR/calculate_effect.py
@@ -1,12 +1,13 @@
 import polars as pl
+from scipy.optimize import minimize
+import numpy as np
 
-"""
-iwv: Inverse Weighted Variance
-wr: Wald Ratio
-ml: Maximum Likelihood
-wm: Weighed Median
-sm: Simple Median
-"""
+
+# iwv: Inverse Weighted Variance
+# wr: Wald Ratio
+# ml: Maximum Likelihood
+# wm: Weighed Median
+# sm: Simple Median
 methods = ['iwv', 'wr', 'ml', 'wm']
 
 
@@ -21,9 +22,22 @@ def calculate_effect(data: pl.DataFrame, method: str):
         se = ((data['beta_exp'] ** 2 *
               data['se_out'] ** -2).sum()) ** -0.5
 
-    elif method == 'wald ratio':
+    elif method == 'wr':
         effect = (data['beta_out'] / data['beta_exp']).mean()
         se = (data['se_out'] / abs(data['beta_exp'])).mean()
+
+    elif method == 'ml':
+        n = data.shape[0]
+
+        def log_likelihood(param):
+            return 1/2 * ((data['beta_exp'] - param[0:n])**2/data['se_exp']**2).sum() + 1/2 * ((data['beta_out'] - param[n] * param[0:n])**2 / data['se_out']**2).sum()
+
+        initial = np.append((data['beta_exp'].to_numpy()), (data['beta_exp']*data['beta_out'] /
+                                                            data['se_out']**2).sum()/(data['beta_exp']**2/data['se_out']**2).sum())
+
+        res = minimize(log_likelihood, initial)
+        effect = res.x[n]
+        se = res.hess_inv[n, n] ** 1/2
 
     return {
         'effect': effect, 'se': se
