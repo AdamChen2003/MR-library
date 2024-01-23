216a572b09b399108f1a77be47eae4d614fbe788
Adam Chen <adamchen2301@gmail.com>
2024-01-23 23:42:20 +1100

    Updated documentation

--- MR/mr_methods/mr_median.py
+++ MR/mr_methods/mr_median.py
@@ -3,8 +3,17 @@ from statistics import stdev
 from scipy.stats import chi2
 
 
-def weighted_median(b_iv, weights):
-    beta_IV_sorted = np.sort(b_iv)
+def mr_median(beta_iv, weights):
+    """
+    Weighted median method.
+
+    Arguments:
+
+    beta_iv -- Wald Ratios
+
+    weights -- Weights for each SNP
+    """
+    beta_IV_sorted = np.sort(beta_iv)
     weights_sorted = np.sort(weights)
     weights_sum = np.cumsum(weights_sorted) - 1/2 * weights_sorted
     weights_sum = weights_sum/np.sum(weights_sorted)
@@ -13,7 +22,24 @@ def weighted_median(b_iv, weights):
         1/2-weights_sum[below-1])/(weights_sum[below]-weights_sum[below-1])
 
 
-def weighted_median_se(beta_exp, beta_out, se_exp, se_out, weights, nboot=1000):
+def mr_median_se(beta_exp, beta_out, se_exp, se_out, weights, nboot=1000):
+    """
+    Computes the SE for median methods.
+
+    Arguments:
+
+    beta_exp -- Vector of genetic effects on exposure
+
+    beta_out -- Vector of genetic effects on outcome
+
+    se_exp -- Standard errors of genetic effects on exposure
+
+    se_out -- Standard errors of genetic effects on outcome
+
+    weights -- Weights of each SNP
+
+    nboot -- Number of bootstraps to calculate SE
+    """
     med = []
     for i in range(1, nboot + 1):
         beta_exp_boot = np.random.normal(
@@ -21,12 +47,12 @@ def weighted_median_se(beta_exp, beta_out, se_exp, se_out, weights, nboot=1000):
         beta_out_boot = np.random.normal(
             loc=beta_out, scale=se_out, size=len(beta_out))
         betaIV_boot = beta_out_boot/beta_exp_boot
-        med.append(weighted_median(betaIV_boot, weights))
+        med.append(mr_median(betaIV_boot, weights))
 
     return stdev(med)
 
 
-def mr_simple_median(beta_exp, beta_out, se_exp, se_out):
+def mr_simple_median(beta_exp, beta_out, se_exp, se_out, nboot=1000):
     """
     Computes the causal effect using simple median.
 
@@ -39,19 +65,21 @@ def mr_simple_median(beta_exp, beta_out, se_exp, se_out):
     se_exp -- Standard errors of genetic effects on exposure
 
     se_out -- Standard errors of genetic effects on outcome
+
+    nboot -- Number of bootstraps to calculate SE
     """
     n = len(beta_exp)
-    b_iv = beta_out/beta_exp
-    effect = weighted_median(b_iv, np.repeat(1/n, n))
-    se = weighted_median_se(beta_exp, beta_out, se_exp,
-                            se_out, np.repeat(1/n, n))
+    beta_iv = beta_out/beta_exp
+    effect = mr_median(beta_iv, np.repeat(1/n, n))
+    se = mr_median_se(beta_exp, beta_out, se_exp,
+                      se_out, np.repeat(1/n, n), nboot)
 
     return {
         'effect': effect, 'se': se
     }
 
 
-def mr_weighted_median(beta_exp, beta_out, se_exp, se_out):
+def mr_weighted_median(beta_exp, beta_out, se_exp, se_out, nboot=1000):
     """
     Computes the causal effect using weighted median.
 
@@ -64,18 +92,20 @@ def mr_weighted_median(beta_exp, beta_out, se_exp, se_out):
     se_exp -- Standard errors of genetic effects on exposure
 
     se_out -- Standard errors of genetic effects on outcome
+
+    nboot -- Number of bootstraps to calculate SE
     """
-    b_iv = beta_out/beta_exp
+    beta_iv = beta_out/beta_exp
     VBj = se_out**2/beta_exp**2 + beta_out**2 * se_exp**2/beta_exp**4
-    effect = weighted_median(b_iv, 1/VBj)
-    se = weighted_median_se(beta_exp, beta_out, se_exp, se_out, 1/VBj)
+    effect = mr_median(beta_iv, 1/VBj)
+    se = mr_median_se(beta_exp, beta_out, se_exp, se_out, 1/VBj, nboot)
 
     return {
         'effect': effect, 'se': se
     }
 
 
-def mr_penalised_weighted_median(beta_exp, beta_out, se_exp, se_out):
+def mr_penalised_weighted_median(beta_exp, beta_out, se_exp, se_out, nboot=1000):
     """
     Computes the causal effect using penalised weighted median.
 
@@ -88,6 +118,8 @@ def mr_penalised_weighted_median(beta_exp, beta_out, se_exp, se_out):
     se_exp -- Standard errors of genetic effects on exposure
 
     se_out -- Standard errors of genetic effects on outcome
+
+    nboot -- Number of bootstraps to calculate SE
     """
     beta_iv = beta_out/beta_exp
     beta_ivw = ((beta_out*beta_exp*se_out**(-2)).sum() /
@@ -96,10 +128,11 @@ def mr_penalised_weighted_median(beta_exp, beta_out, se_exp, se_out):
     weights = 1/VBj
     bwm = mr_weighted_median(beta_exp, beta_out, se_exp, se_out)
     penalty = chi2.cdf(weights*beta_iv-bwm['effect']**2, df=1)
+    # penalty * pmin(weights, 1)
     penalty_weights = penalty*weights
-    effect = weighted_median(beta_iv, penalty_weights)
-    se = weighted_median_se(beta_exp, beta_out, se_exp,
-                            se_out, penalty_weights)
+    effect = mr_median(beta_iv, penalty_weights)
+    se = mr_median_se(beta_exp, beta_out, se_exp,
+                      se_out, penalty_weights, nboot)
 
     return {
         'effect': effect, 'se': se
