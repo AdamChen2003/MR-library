821c37520ea6ad987f2b4e984c67d7d72519e150
Adam Chen <adamchen2301@gmail.com>
2024-01-31 23:36:04 +1100

    Changed method to use WLS and also added p-value

--- MR/mr_methods/inverse_variance_weighted.py
+++ MR/mr_methods/inverse_variance_weighted.py
@@ -1,3 +1,7 @@
+from scipy import stats
+import statsmodels.api as sm
+
+
 def mr_inverse_variance_weighted(beta_exp, beta_out, se_out):
     """
     Computes the causal effect using inverse weighted variance.
@@ -17,10 +21,20 @@ def mr_inverse_variance_weighted(beta_exp, beta_out, se_out):
         'se' : standard error of effect estimation
     }
     """
-    effect = (beta_exp*beta_out * se_out**(-2)).sum() / \
-        (beta_exp**2*se_out**(-2)).sum()
-    se = ((beta_exp**2*se_out**(-2)).sum())**(-0.5)
+    mod = sm.WLS(beta_out.to_numpy(), beta_exp.to_numpy(),
+                 weights=1 / (se_out.to_numpy() ** 2)).fit()
+
+    effect = mod.params[0]
+    se = mod.bse[0]/min(1, mod.scale)
+    pval = 2*(1-stats.norm.cdf(abs(effect/se)))
 
     return {
-        'effect': effect, 'se': se
+        'effect': effect, 'se': se, 'pval': pval
     }
+    # effect = (beta_exp*beta_out * se_out**(-2)).sum() / \
+    #     (beta_exp**2*se_out**(-2)).sum()
+    # se = ((beta_exp**2*se_out**(-2)).sum())**(-0.5)
+
+    # return {
+    #     'effect': effect, 'se': se
+    # }
