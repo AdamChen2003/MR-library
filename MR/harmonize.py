38bd39879643c3acc341f2e5f894d2457f797993
Adam Chen <adamchen2301@gmail.com>
2024-01-31 21:32:59 +1100

    Added palindromic_action parameter to harmonize function

--- MR/harmonize.py
+++ MR/harmonize.py
@@ -1,7 +1,7 @@
 import polars as pl
 
 
-def harmonize(data: pl.DataFrame, palindromic_threshold=0.05):
+def harmonize(data: pl.DataFrame, palindromic_action=1, palindromic_threshold=0.08):
     """
     Harmonizes the data based on this article
     https://mrcieu.github.io/TwoSampleMR/articles/harmonise.html#palindromic-snp-inferrable
@@ -79,29 +79,43 @@ def harmonize(data: pl.DataFrame, palindromic_threshold=0.05):
                        reverse_flipped[sorted(reverse_flipped.columns)]])
 
     # Dealing with palindromic SNPs
-    palindromic = total.filter(
-        ((pl.col('ea_exp') == 'a') & (pl.col('oa_exp') == 't')) |
-        ((pl.col('ea_exp') == 't') & (pl.col('oa_exp') == 'a')) |
-        ((pl.col('ea_exp') == 'g') & (pl.col('oa_exp') == 'c')) |
-        ((pl.col('ea_exp') == 'c') & (pl.col('oa_exp') == 'g')))
 
-    total = total.filter(
-        ~(((pl.col('ea_exp') == 'a') & (pl.col('oa_exp') == 't')) |
-          ((pl.col('ea_exp') == 't') & (pl.col('oa_exp') == 'a')) |
-          ((pl.col('ea_exp') == 'g') & (pl.col('oa_exp') == 'c')) |
-          ((pl.col('ea_exp') == 'c') & (pl.col('oa_exp') == 'g'))))
+    if palindromic_action == 1:
+        # Keep the SNPs which meet the thresholds and discard the rest
+        palindromic = total.filter(
+            ((pl.col('ea_exp') == 'a') & (pl.col('oa_exp') == 't')) |
+            ((pl.col('ea_exp') == 't') & (pl.col('oa_exp') == 'a')) |
+            ((pl.col('ea_exp') == 'g') & (pl.col('oa_exp') == 'c')) |
+            ((pl.col('ea_exp') == 'c') & (pl.col('oa_exp') == 'g')))
 
-    # For all SNPs where effect allele freq is greater than threshold + 0.5 and minor allele freq is less than 0.5 - threshold, flip the beta effect.
-    # For all SNPs where both effect ellele freq and minor allele freq are greater than 0.5 + threshold or less than 0.5 - threshold, change nothing.
-    # Otherwise, discard the SNP.
+        total = total.filter(
+            ~(((pl.col('ea_exp') == 'a') & (pl.col('oa_exp') == 't')) |
+              ((pl.col('ea_exp') == 't') & (pl.col('oa_exp') == 'a')) |
+                ((pl.col('ea_exp') == 'g') & (pl.col('oa_exp') == 'c')) |
+                ((pl.col('ea_exp') == 'c') & (pl.col('oa_exp') == 'g'))))
 
-    correct_palindromic = palindromic.filter((((pl.col('eaf_exp') > 0.5 + palindromic_threshold) & (pl.col('eaf_out') > 0.5 + palindromic_threshold)) |
-                                              ((pl.col('eaf_exp') < 0.5 - palindromic_threshold) & (pl.col('eaf_out') < 0.5 - palindromic_threshold))))
+        # For all SNPs where effect allele freq is greater than threshold + 0.5 and minor allele freq is less than 0.5 - threshold, flip the beta effect.
+        # For all SNPs where both effect ellele freq and minor allele freq are greater than 0.5 + threshold or less than 0.5 - threshold, change nothing.
+        # Otherwise, discard the SNP.
 
-    flipped_palindromic = (palindromic.filter(((pl.col('eaf_exp') > 0.5 + palindromic_threshold) & (pl.col('eaf_out') < 0.5 - palindromic_threshold)))
-                           .with_columns(
-        # Flip the signs of the outcome effects
-        pl.col('beta_out').mul(-1))
-    )
+        correct_palindromic = palindromic.filter((((pl.col('eaf_exp') > 0.5 + palindromic_threshold) & (pl.col('eaf_out') > 0.5 + palindromic_threshold)) |
+                                                  ((pl.col('eaf_exp') < 0.5 - palindromic_threshold) & (pl.col('eaf_out') < 0.5 - palindromic_threshold))))
 
-    return pl.concat([total, correct_palindromic, flipped_palindromic])
+        flipped_palindromic = (palindromic.filter(((pl.col('eaf_exp') > 0.5 + palindromic_threshold) & (pl.col('eaf_out') < 0.5 - palindromic_threshold)))
+                               .with_columns(
+            # Flip the signs of the outcome effects
+            pl.col('beta_out').mul(-1))
+        )
+
+        return pl.concat([total, correct_palindromic, flipped_palindromic])
+
+    elif palindromic_action == 2:
+        # Get rid of all palindromic SNPs
+        return total.filter(
+            ~(((pl.col('ea_exp') == 'a') & (pl.col('oa_exp') == 't')) |
+              ((pl.col('ea_exp') == 't') & (pl.col('oa_exp') == 'a')) |
+              ((pl.col('ea_exp') == 'g') & (pl.col('oa_exp') == 'c')) |
+              ((pl.col('ea_exp') == 'c') & (pl.col('oa_exp') == 'g'))))
+
+    elif palindromic_action == 3:
+        return total
