27a21fca8211a6a014434050bb88b6d55ff29659
Adam Chen <adamchen2301@gmail.com>
2024-02-02 23:58:13 +1100

    Moved pmin function to utils

--- MR/mr_methods/utils.py
+++ MR/mr_methods/utils.py
@@ -2,8 +2,18 @@ import numpy as np
 
 
 def pmin(x1, x2):
+    """
+    Computes the parallel minima between two vectors
+    """
     arr = np.array([])
     for i in range(0, len(x1)):
         arr = np.append(arr, min(x1[i], x2[i]))
 
     return arr
+
+
+def mad(data):
+    """
+    Computes median absolute deivation for provided data
+    """
+    return (abs(data-data.mean())).sum()/len(data)
